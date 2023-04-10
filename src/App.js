5836f73961679c3f07dcafa84fd8f639a2980dd1
nitin <nimble2905@gmail.com>
2023-04-10 22:18:45 +0530

    added some function using usestate

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import React, {useState} from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
-// import Textform from './Components/Textform';
-import About from './Components/About';
+import Textform from './Components/Textform';
+// import About from './Components/About';
 
 function App() {
+  const[mode, setMode]=useState('light');
+  const[buttonMode, setButtonMode]=useState('dark')
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      setButtonMode('light')
+      document.body.style.backgroundColor='darkgray';
+    }
+    else{
+      setMode('light');
+      setButtonMode('dark')
+      document.body.style.backgroundColor='white';
+    }
+  }
   return (
     <>
-    <Navbar title='TextUtils'/>
-    <About/>
-    {/* <Textform heading='Enter your text to analyze'/> */}
+    <Navbar title='TextUtils' toggleMode={toggleMode} mode={mode} buttonMode={buttonMode} />
+    {/* <About/> */}
+    <Textform heading='Enter your text to analyze' mode={mode}/>
     </>
   );
 }
