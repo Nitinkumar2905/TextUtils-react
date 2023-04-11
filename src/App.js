ced7dcbad0be47bd29923d9f4460d6741933c196
nitin <nimble2905@gmail.com>
2023-04-11 20:32:01 +0530

    added autoalert function

--- src/App.js
+++ src/App.js
@@ -2,28 +2,39 @@ import React, {useState} from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Textform from './Components/Textform';
+import Alert from './Components/Alert';
 // import About from './Components/About';
 
 function App() {
   const[mode, setMode]=useState('light');
-  const[buttonMode, setButtonMode]=useState('dark')
   const toggleMode=()=>{
     if(mode==='light'){
       setMode('dark');
-      setButtonMode('light')
+      showAlert('Dark mode has been enabled', 'success')
       document.body.style.backgroundColor='darkgray';
     }
     else{
       setMode('light');
-      setButtonMode('dark')
+      showAlert('Light mode has been enabled', 'success')
       document.body.style.backgroundColor='white';
     }
   }
+  const[alert, setAlert]=useState(null)
+  const showAlert=(message, type)=>{
+    setAlert({
+      msg : message,
+      type : type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    },1000);
+  }
   return (
     <>
-    <Navbar title='TextUtils' toggleMode={toggleMode} mode={mode} buttonMode={buttonMode} />
-    {/* <About/> */}
-    <Textform heading='Enter your text to analyze' mode={mode}/>
+      <Navbar title='TextUtils' toggleMode={toggleMode} mode={mode}/>
+      {/* <About/> */}
+      <Alert className='sticky-top' alert={alert}/>
+      <Textform showAlert={showAlert} heading='Enter your text to analyze' mode={mode}/>
     </>
   );
 }
